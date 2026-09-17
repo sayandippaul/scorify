cc06c8767a102ab92d39c96034778baad45a53da
sayandippaul <sayandip126@gmail.com>
2026-09-18 02:56:55 +0530

    fixed app not conneted

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./app.jsx";
+import App from "./App.jsx";
 import "./style.css";
 
 createRoot(document.getElementById("root")).render(
